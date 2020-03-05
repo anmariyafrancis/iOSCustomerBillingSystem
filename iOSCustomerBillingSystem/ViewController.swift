f1132625423b4cede8010ccf75e7be8f14ef23ae
anmariyafrancis <anmariya1915@gmail.com>
2020-03-05 18:04:49 -0500

    [ANMARIYA] Doing laughscreen designing

---
+++ iOSCustomerBillingSystem/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOSCustomerBillingSystem
+//
+//  Created by MacStudent on 2020-03-05.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
