24cbebf76a398da938077d908e2f2cfa2648dc7e
anmariyafrancis <anmariya1915@gmail.com>
2020-03-06 19:06:15 -0500

    [ANMARIYA] Created AddNewCustomerViewController.swift file

---
+++ iOSCustomerBillingSystem/AddNewCustomerViewController AddNewCustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewCustomerViewController AddNewCustomerViewController.swift
+//  iOSCustomerBillingSystem
+//
+//  Created by MacStudent on 2020-03-06.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class AddNewCustomerViewController_AddNewCustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
