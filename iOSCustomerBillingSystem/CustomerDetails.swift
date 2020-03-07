e3f628de3773ab8649c562b843a023ccea1227bd
anmariyafrancis <anmariya1915@gmail.com>
2020-03-07 18:58:19 -0500

    [ANMARIYA] Created customerDeatils swift class

---
+++ iOSCustomerBillingSystem/CustomerDetails.swift
@@ -0,0 +1,31 @@
+//
+//  CustomerDetails.swift
+//  iOSCustomerBillingSystem
+//
+//  Created by MacStudent on 2020-03-07.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customers
+{
+    var customerId:String
+    var firstName:String
+    var lastName:String
+    var email:String
+    var password:String
+    var fullName:String
+    {
+        return "\(firstName) \(lastName)"
+    }
+    
+    init(customerId:String,firstName:String,lastName:String,email:String,password:String)
+    {
+        self.customerId=customerId
+        self.firstName=firstName
+        self.lastName=lastName
+        self.email=email
+        self.password=password
+    }
+}
